b59a7498d602d00887587705e1c0161c1023fadb
DANIEL GIL <78768844+dannieowk@users.noreply.github.com>
2021-06-02 20:14:15 -0500

    update IP

--- api/src/utils/middlewares/setHandlers.js
+++ api/src/utils/middlewares/setHandlers.js
@@ -1,5 +1,5 @@
 function setHeaders(req, res, next){
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // ! ACTUALIZAR CON EL DOMINIO DEL FORNTEND
+    res.header('Access-Control-Allow-Origin', 'http://143.198.164.43'); // ! ACTUALIZAR CON EL DOMINIO DEL FORNTEND
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
