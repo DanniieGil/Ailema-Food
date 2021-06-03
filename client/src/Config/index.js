f81fa3802b89e26de08fae81caed7af5427f7c17
DANIEL GIL <78768844+dannieowk@users.noreply.github.com>
2021-06-02 20:15:15 -0500

    update IP

--- client/src/Config/index.js
+++ client/src/Config/index.js
@@ -1,5 +1,5 @@
 // export const RECIPE_URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.API_KEY}&addRecipeInformation=true`;
 // export const RECIPE_URL = `https://rickandmortyapi.com/api/episode`;
-export const RECIPE_URL = `http://143.198.164.43//recipes`;
+export const RECIPE_URL = `http://143.198.164.43/recipes`;
 export const RECIPE_DETAIL_URL = `http://143.198.164.43/recipes/information/`;
 export const DIET_URL =  `http://143.198.164.43/diets`;
